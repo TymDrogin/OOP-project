4ceed2060759f43c85edc46f058e996d0d97f975
TymDrogin <tim.drogin@gmail.com>
2023-11-24 16:58:17 +0000

    stage

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -1,5 +1,26 @@
 #pragma once
-constexpr int APP_HOUSING_ROWS = 4;
-constexpr int APP_HOUSING_COLUMNS = 4;
+
+
+//GRID SETTINGS
+
+// HOUSING GRID SIZE CONTROL
+constexpr int APP_HOUSING_ROWS = 3;
+constexpr int APP_HOUSING_COLUMNS = 3;
+
+// OFFSET FROM THE ORIGIN
+constexpr int APP_HOUSING_OFFSET_X = 4;
+constexpr int APP_HOUSING_OFFSET_Y = 1;
+
+// SINGLE TILE DIMENSIONS
+constexpr int APP_HOUSING_TILE_HEIGHT = 5;
+constexpr int APP_HOUSING_TILE_WIDTH = 10;
+
+// TILE SPACING
+constexpr int APP_HOUSING_TILE_SPACING_X = 1;
+constexpr int APP_HOUSING_TILE_SPACING_Y = 0;
+
+
+// GLOBAL APP SETTINGS
+// APP SETTINGS
 constexpr int APP_WIDTH = 80;
 constexpr int APP_HEIGHT = 24;
